1814a9e27aae90e519852c7efe662be9505e74fd
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-12-08 17:25:28 +1300

    FIX: axios baseUrl fixed for SSL purpose

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://3.25.119.92:4444",
+    baseURL: "https://corbenykt.ru:4444",
 });
 
 instance.interceptors.request.use((config) => {
