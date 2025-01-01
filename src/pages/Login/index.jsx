814d4e0e3b72ac9bea2aba9be1ea7b58ffd7768c
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2025-01-02 11:54:32 +1300

    UPD: made login and registration forms more fancy

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
 import { useState } from 'react'
 
@@ -33,41 +33,59 @@ export const Login = () => {
   }
 
   return (
-    <div className="w-full max-w-md mx-auto p-6">
-      <h2 className="text-2xl font-semibold mb-6">Log In</h2>
-      {errorMessage && (
-        <div className="mb-4 text-sm text-red-600">
-          {errorMessage}
-        </div>
-      )}
+    <div className="w-full max-w-md mx-auto">
+      <section className="dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-4 mx-auto lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Log in
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                  <input
+                    id="email"
+                    type="email"
+                    className={`w-full p-2 border border-gray-300 rounded-md ${errors.email ? 'border-red-500' : ''}`}
+                    {...register('email', { required: 'Enter email' })}
+                    placeholder="Enter your email"
+                  />
+                  {errors.email && <p className="text-sm text-red-500 mt-1">{errors.email.message}</p>}
+                </div>
+                <div>
+                  <input
+                    id="password"
+                    type="password"
+                    className={`w-full p-2 border border-gray-300 rounded-md ${errors.password ? 'border-red-500' : ''}`}
+                    {...register('password', { required: 'Enter password' })}
+                    placeholder="Enter your password"
+                  />
+                  {errors.password && <p className="text-sm text-red-500 mt-1">{errors.password.message}</p>}
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                  disabled={!isValid}
+                >
+                  Log In
+                </button>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-6">
-          <label className="block text-sm font-medium text-gray-700" htmlFor="email">E-Mail</label>
-          <input
-            id="email"
-            type="email"
-            className={`w-full p-2 border border-gray-300 rounded-md ${errors.email ? 'border-red-500' : ''}`}
-            {...register('email', { required: 'Enter email' })}
-          />
-          {errors.email && <p className="text-sm text-red-500 mt-1">{errors.email.message}</p>}
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{' '}
+                  <Link to="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Create Account
+                  </Link>
+                </p>
+                {errorMessage && (
+                  <div className="mb-4 text-sm text-red-600">
+                    {errorMessage}
+                  </div>
+                )}
+              </form>
+            </div>
+          </div>
         </div>
-
-        <div className="mb-6">
-          <label className="block text-sm font-medium text-gray-700" htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            className={`w-full p-2 border border-gray-300 rounded-md ${errors.password ? 'border-red-500' : ''}`}
-            {...register('password', { required: 'Enter password' })}
-          />
-          {errors.password && <p className="text-sm text-red-500 mt-1">{errors.password.message}</p>}
-        </div>
-
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled={!isValid}>
-          Log In
-        </button> *For testing just push the button
-      </form>
+      </section>
     </div>
   );
 };
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -57,87 +57,87 @@ export const Registration = () => {
   }
 
   return (
-    <div className="w-full max-w-md mx-auto p-6">
+    <div className="w-full max-w-md mx-auto">
+      <section className="dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-4 mx-auto lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
-      <h1 className="text-2xl font-bold text-center mb-4">Create Account</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <div className="flex justify-center items-center">
-          <div className="relative">
-            <div
-              className="w-32 h-32 bg-gray-200 rounded-full overflow-hidden cursor-pointer shadow-md flex justify-center items-center"
-              onClick={() => inputFileRef.current.click()}
-            >
-              {avatarString ? (
-                <img
-                  src={avatarString}
-                  alt="Avatar Preview"
-                  className="w-full h-full object-cover"
-                />
-              ) : (
-                <span className="text-gray-500">Upload</span>
-              )}
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create Account</h1>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                <div className="flex justify-center items-center">
+                  <div className="relative">
+                    <div
+                      className="w-32 h-32 bg-gray-200 rounded-full overflow-hidden cursor-pointer shadow-md flex justify-center items-center"
+                      onClick={() => inputFileRef.current.click()}
+                    >
+                      {avatarString ? (
+                        <img
+                          src={avatarString}
+                          alt="Avatar Preview"
+                          className="w-full h-full object-cover"
+                        />
+                      ) : (
+                        <span className="text-gray-500">Upload</span>
+                      )}
+                    </div>
+
+                    <input
+                      ref={inputFileRef}
+                      type="file"
+                      className="hidden"
+                      onChange={handleAvatarUpload}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <input
+                    {...register('fullName', { required: 'Enter the full name' })}
+                    className={`w-full p-2 border border-gray-300 rounded-md ${errors.fullName ? 'border-red-500' : ''
+                      } `}
+                    placeholder="Enter your full name"
+                  />
+                  <p className="text-red-500 text-xs mt-1">{errors.fullName?.message}</p>
+                </div>
+                <div>
+                  <input
+                    {...register('email', { required: 'Enter the email' })}
+                    className={`w-full p-2 border border-gray-300 rounded-md ${errors.email ? 'border-red-500' : ''
+                      } `}
+                    placeholder="Enter your email"
+                  />
+                  <p className="text-red-500 text-xs mt-1">{errors.email?.message}</p>
+                </div>
+                <div>
+                  <input
+                    {...register('password', { required: 'Enter the password' })}
+                    type="password"
+                    className={`w-full p-2 border border-gray-300 rounded-md ${errors.password ? 'border-red-500' : ''
+                      } `}
+                    placeholder="Enter your password"
+                  />
+                  <p className="text-red-500 text-xs mt-1">{errors.password?.message}</p>
+                </div>
+                {serverError && (
+                  <div className="text-red-500 text-sm text-center">
+                    {serverError}
+                  </div>
+                )}
+                <button
+                  type="submit"
+                  className={`w-full py-2 px-4 text-white font-bold rounded-md ${isValid ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-gray-400 cursor-not-allowed'
+                    }`}
+                  disabled={!isValid}
+                >
+                  Register
+                </button>
+              </form>
             </div>
-
-            <input
-              ref={inputFileRef}
-              type="file"
-              className="hidden"
-              onChange={handleAvatarUpload}
-            />
           </div>
         </div>
-
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Full Name
-          </label>
-          <input
-            {...register('fullName', { required: 'Enter the full name' })}
-            className={`mt-1 block w-full px-3 py-2 border ${errors.fullName ? 'border-red-500' : 'border-gray-300'
-              } rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-            placeholder="Enter your full name"
-          />
-          <p className="text-red-500 text-xs mt-1">{errors.fullName?.message}</p>
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            E-Mail
-          </label>
-          <input
-            {...register('email', { required: 'Enter the email' })}
-            className={`mt-1 block w-full px-3 py-2 border ${errors.email ? 'border-red-500' : 'border-gray-300'
-              } rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-            placeholder="Enter your email"
-          />
-          <p className="text-red-500 text-xs mt-1">{errors.email?.message}</p>
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Password
-          </label>
-          <input
-            {...register('password', { required: 'Enter the password' })}
-            type="password"
-            className={`mt-1 block w-full px-3 py-2 border ${errors.password ? 'border-red-500' : 'border-gray-300'
-              } rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-            placeholder="Enter your password"
-          />
-          <p className="text-red-500 text-xs mt-1">{errors.password?.message}</p>
-        </div>
-        {serverError && (
-          <div className="text-red-500 text-sm text-center">
-            {serverError}
-          </div>
-        )}
-        <button
-          type="submit"
-          className={`w-full py-2 px-4 text-white font-bold rounded-md ${isValid ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-gray-400 cursor-not-allowed'
-            }`}
-          disabled={!isValid}
-        >
-          Register
-        </button>
-      </form>
+      </section>
     </div>
   );
 };
